99e097af70a8c125c76213b078e85a6d6de01b8a
WeslyM003 <maulsbywes@gmail.com>
2023-11-30 09:07:15 -0600

    Working Conversion, buggy formatting.

--- Wesly Maulsby - Lab 6 - Binary to Decimal/main.cpp
+++ Wesly Maulsby - Lab 6 - Binary to Decimal/main.cpp
@@ -1,17 +1,28 @@
+/********************
+Wesly Maulsby
+Lab 6
+Binary Input
+Convert Binary input to Base 10 output.
+********************/
+
+//necessary files
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 
+//infile
 string inputFileName = "BinaryIn.dat";
 
 int main()
 {
-	cout << "Binary Input                                                    ||  Decimal Output   \n";
+	//output header
+	cout << "Binary Input                                                ||  Decimal Output   \n";
 	// 13 11 15
 	ifstream infile;
 	infile.open(inputFileName);
 
+	//initializes values
 	char input;
 	int base10 = 0;
 	bool firstDigit = false;
@@ -22,12 +33,14 @@ int main()
 
 	infile.get(input);
 	
+	//reads input digit by digit for binary conversion
 	while (infile)
 	{
 		while (input != '\n' && infile)
 		{
 			if (!firstDigit && input == '1' || firstDigit && (input == '1' || input == '0'))
 			{
+				//binary conversion
 				spaces = spaces + 1;
 				count++;
 				cout << input;
@@ -38,23 +51,33 @@ int main()
 				{
 					base10++;
 				}
-			}
+			} //bad digit on input check
 			else if (input != ' ' || input == ' ' && firstDigit)
 			{
 				for (int i = 0; i < count; i++)
 					cout << '\b';
-				cout << "bad digit on input                                              ||\n";
+				cout << "bad digit on input                                          ||\n";
 				badDigit = true;
 				infile.ignore(256, '\n');
 				break;
 			}
 			infile.get(input);
 		}
-		
+		//if number is valid then it outputs the answer
 		if (!badDigit)
 			for (int i = 0; i < spaces; i++)
-				space += '\b';
-			cout << "                                                           " << space << "||  " << base10 << endl;
+				//spaces after binary input
+				cout << "                                                           ";
+		        //should loop for the amout of times that a number was read in for a binary input, stored in "spaces"
+				while (spaces > 0)
+				{
+					cout << "\b";
+					spaces = spaces - 1;
+				}
+				//outputs the rest of the output
+				cout << space << "||  " << base10 << endl;
+		
+		//resets variables for next input
 		firstDigit = false;
 		badDigit = false;
 		count = 0;
